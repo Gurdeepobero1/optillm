2481d759bcaa83fcac26f23aa613311736f46103
Gurdeep <111205020+Gurdeepobero1@users.noreply.github.com>
2026-03-19 11:29:24 +0530

    hhh

--- core/cache.py
+++ core/cache.py
@@ -1,29 +1,11 @@
-import faiss
-import numpy as np
-from core.embed import get_embedding
-
-dim = 384
-index = faiss.IndexFlatL2(dim)
-stored_data = []
+cache_store = {}
 
 def search_cache(query):
-    if index.ntotal == 0:
-        return None
-
-    emb = np.array([get_embedding(query)]).astype("float32")
-    D, I = index.search(emb, 1)
-
-    if D[0][0] < 0.3:
-        return stored_data[I[0][0]][1]
-
-    return None
+    return cache_store.get(query)
 
 def add_to_cache(query, response):
-    emb = np.array([get_embedding(query)]).astype("float32")
-    index.add(emb)
-    stored_data.append((query, response))
-    
+    cache_store[query] = response
+
 def clear_cache():
-    global index, stored_data
-    index.reset()
-    stored_data = []
+    global cache_store
+    cache_store = {}
